86d4bf1b9de1cf250c9894c4455815baa048dd32
Matthijs Boet <m.boet@chello.nl>
2020-04-07 15:08:38 +0200

    pure react exersises

--- 2-react-basics/monster-rolodex/src/App.js
+++ 2-react-basics/monster-rolodex/src/App.js
@@ -19,6 +19,10 @@ class App extends Component{
       .then(response => response.json())
       .then(users => this.setState({monsters:users}))
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
   
   render() {
     const { monsters, searchField } = this.state;
@@ -29,9 +33,10 @@ class App extends Component{
       );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder='search monsters'
-          handleChange={ e => this.setState({ searchField: e.target.value}) }
+          handleChange={ this.handleChange }
          />
         <CardList monsters={ filteredMonsters } />
       </div>
