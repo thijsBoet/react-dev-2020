7002dbc8467fad9f0ca7853d5b8627e4ea58ee40
Matthijs Boet <m.boet@chello.nl>
2020-04-08 19:16:47 +0200

    finished base app about state

--- 2-react-basics/4-base-app-state/src/App.js
+++ 2-react-basics/4-base-app-state/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meaningOfLife: 46 + this.props.increment
+    };
+
+  }
+
+  handleClick = () => {
+    this.setState((prevState, prevProps) => {
+      return {meaningOfLife: this.state.meaningOfLife + this.props.increment}
+    },
+       () => console.log(this.state.meaningOfLife)
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>The Meaning of Life is: {this.state.meaningOfLife}</p>
+          <button onClick={this.handleClick}>Update State</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
